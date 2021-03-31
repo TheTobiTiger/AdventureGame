ad3ac548725f257291ab650a5b58b51d8b1a7a26
Tobias Daniel Heyn <heynto@noreply.gitlab.int.bszn.schule>
2021-03-31 11:29:24 +0200

    „Figur.java“ ändern

--- Figur.java
+++ Figur.java
@@ -8,21 +8,53 @@ public class Figur {
 	private int hp;
 	private int def;
 	private int magie;
+	private int schild = 2;
 	
-	public Figur() { 
+	public Figur() {
 		
-		fhfhfhfhf
+	}
+	
+	public Figur(int atk, int hp, int def, int magie) {
+		this.atk = atk;
+		this.hp = hp;
+		this.def = def;
+		this.magie = magie;
+		
+		
 	}
 	
 	public void laufen() {
 		
 	}
 	
-	public void angreifen(int atk) {
+	public int angreifen(Figur charakter, Figur gegner) {
+		gegner.hp -= charakter.atk;
+		System.out.println("Gegnerische Leben: " + gegner.hp);
+		return gegner.hp;
 		
 	}
 	
 	public void blocken() {
+		switch(schild) {
+		case 2: 
+			System.out.println("Angriff erfolgreich abgewehrt!");
+			this.schild -=1;
+			System.out.println(schild + " Schild übrig \n");
+			break;
+		case 1:
+			System.out.println("Angriff erfolgreich abgewehrt!");
+			this.schild -=1;
+			System.out.println(schild + " Schilde übrig\n");
+			break;
+		case 0:
+			System.out.println("Du hast keine Schilde mehr übrig!\n");
+			break;
+			
+	}
+				
+	}
+	
+	public int angreifenMagie(Figur charakter, Figur gegner) {
 		
 	}
 	
